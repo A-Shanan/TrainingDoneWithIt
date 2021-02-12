3a3634fbc04fac9a99982c988e7d23e6ba6988fb
A-Shanan <abboudshanan66@gmail.com>
2021-02-12 16:59:38 +0300

    welcomeScreen

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { View,StyleSheet } from "react-native";
+import AppButton from "./app/components/AppButton";
+
+import AppText from "./app/components/AppText";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+  <WelcomeScreen/>
+
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
